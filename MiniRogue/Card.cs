4d575c76b276006007ab6c7bdfbb3a0326466b7e
dolso <dolso@DESKTOP-301UTCF>
2017-03-26 02:14:50 -0400

    hjgk

--- MiniRogue/Card.cs
+++ MiniRogue/Card.cs
@@ -13,6 +13,8 @@ namespace MiniRogue
         // Properties
         public string Name { get; set; }
 
+        public bool Visible { get; set; }
+        
         // Constructors
         public Card(string name)
         {
