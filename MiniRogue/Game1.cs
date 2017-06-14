eed0c60d3cc64f5212643c4f81b5043447c72039
dolso <dolso@DESKTOP-5MRRT4S>
2017-06-14 01:52:08 -0400

    dsfaf

--- MiniRogue/Game1.cs
+++ MiniRogue/Game1.cs
@@ -479,7 +479,7 @@ namespace MiniRogue
                     {
                         if (position.X > 800 && position.X < 1050 && position.Y > 167 && position.Y < 241)
                         {
-                            player = new Player(1, 20, 50, 6, spellIcons);
+                            player = new Player(1, 5, 5, 6, spellIcons);
                             DrawNewHand();
                             gamestate = Gamestate.HACKANDSLASH;
                             player.DungeonArea = 14;
