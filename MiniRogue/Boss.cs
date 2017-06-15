8a588415b53788d82d327efc8dbbe88bd990664a
goorak <goorak@BOX>
2017-06-14 23:17:54 -0400

    hgkjgh

--- MiniRogue/Boss.cs
+++ MiniRogue/Boss.cs
@@ -197,8 +197,8 @@ namespace MiniRogue
                     RewardDie.DrawCombatDie(sBatch);
                     sBatch.Draw(Buttons["Rewards"].ButtonTexture, new Vector2(650, 100), new Rectangle?(), Color.White, 0f, new Vector2(), 1f, SpriteEffects.None, 1);
                     //sBatch.DrawString(dungeonFont, "You Rolled a:  " + TreasureResult, new Vector2(725, 200), Color.White);
-                    sBatch.DrawString(dungeonFont, "Click a spell to remove or", new Vector2(650, 250), Color.White, 0f, new Vector2(), 2f, SpriteEffects.None, 0f);
-                    sBatch.DrawString(dungeonFont, "click done to keep current spells", new Vector2(600, 300), Color.White, 0f, new Vector2(), 2f, SpriteEffects.None, 0f);
+                    sBatch.DrawString(dungeonFont, "Click a spell to remove or", new Vector2(650, 250), Color.White, 0f, new Vector2(), 1f, SpriteEffects.None, 0f);
+                    sBatch.DrawString(dungeonFont, "click done to keep current spells", new Vector2(600, 300), Color.White, 0f, new Vector2(), 1f, SpriteEffects.None, 0f);
                     sBatch.Draw(Buttons["Done Button"].ButtonTexture, new Vector2(770, 600), new Rectangle?(), Color.White, 0f, new Vector2(), .75f, SpriteEffects.None, 1);
 
                     break;
